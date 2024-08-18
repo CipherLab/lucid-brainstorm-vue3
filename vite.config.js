c312298ae92ecbd749cb50d3dd51c3f7fd5afae5
Mathew Newport <Mathew Newport>
2024-08-18 12:16:44 -0500

    chore: Add external dependency 'moment' to rollupOptions in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['moment'],
+    },
+  },
   // Add more configuration options as needed (see Vite docs)
 });
