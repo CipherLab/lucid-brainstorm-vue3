a9b03368d60c5be631eb638091ff08bff8e6a079
Mathew Newport <mnewps@live.com>
2024-08-29 13:50:53 -0500

    qmarkdown

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -11,18 +11,19 @@
           name="error"
           color="red"
         />
-
-        <template v-else v-for="(part, index) in messageParts" :key="index">
-          <div v-if="part.isCode">
-            <q-markdown>
-              <pre>
+        <template v-else>
+          <q-markdown>
             {{ props.message }}
-            </pre
-              >
-            </q-markdown>
-          </div>
-          <div>{{ part.content }}</div>
+          </q-markdown>
         </template>
+        <!-- <template v-else v-for="(part, index) in messageParts" :key="index">
+          <CodeBlockWithCopy v-if="part.isCode" :code="part.content" />
+          <div
+            class="message-part"
+            v-else
+            v-html="md.render(part.content)"
+          ></div>
+        </template> -->
       </q-item-label>
       <q-item-label caption class="text-grey-8 text-right">
         {{ formattedTime }}
